071721008fc865dcc0c2927865a0312964e7464f
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-02-02 00:21:22 +0200

    Updating configs

--- drivers/uv_shakira/x8uv.c
+++ drivers/uv_shakira/x8uv.c
@@ -17,7 +17,7 @@
 #define DXDBG(x)					
 
 #define DEVICE_NAME				"X8"
-#define OFS_KALLSYMS_LOOKUP_NAME 0xc00aeca8
+#define OFS_KALLSYMS_LOOKUP_NAME 0xc00aecdc
 
 // struct definition
 struct clkctl_acpu_speed_dx {
