a614055b228252fa25761869d0fa20daa2fba560
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-05-04 00:59:32 +0300

    logger: backporting from CAF 3.0 kernel

--- drivers/staging/android/logger.h
+++ drivers/staging/android/logger.h
@@ -1,7 +1,6 @@
 /* include/linux/logger.h
  *
  * Copyright (C) 2007-2008 Google, Inc.
- * Copyright (c) 2009, Code Aurora Forum. All rights reserved.
  * Author: Robert Love <rlove@android.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -33,6 +32,7 @@ struct logger_entry {
 
 #define LOGGER_LOG_RADIO	"log_radio"	/* radio-related messages */
 #define LOGGER_LOG_EVENTS	"log_events"	/* system/hardware events */
+#define LOGGER_LOG_SYSTEM	"log_system"	/* system/framework messages */
 #define LOGGER_LOG_MAIN		"log_main"	/* everything else */
 
 #define LOGGER_ENTRY_MAX_LEN		(4*1024)
@@ -45,32 +45,5 @@ struct logger_entry {
 #define LOGGER_GET_LOG_LEN		_IO(__LOGGERIO, 2) /* used log len */
 #define LOGGER_GET_NEXT_ENTRY_LEN	_IO(__LOGGERIO, 3) /* next entry len */
 #define LOGGER_FLUSH_LOG		_IO(__LOGGERIO, 4) /* flush log */
-
-#ifdef __KERNEL__
-enum {
-    LOG_PRIORITY_UNKNOWN = 0,
-    LOG_PRIORITY_DEFAULT,    /* only for SetMinPriority() */
-    LOG_PRIORITY_VERBOSE,
-    LOG_PRIORITY_DEBUG,
-    LOG_PRIORITY_INFO,
-    LOG_PRIORITY_WARN,
-    LOG_PRIORITY_ERROR,
-    LOG_PRIORITY_FATAL,
-    LOG_PRIORITY_SILENT,     /* only for SetMinPriority(); must be last */
-};
-
-enum logidx {
-	LOG_MAIN_IDX = 0,
-	LOG_RADIO_IDX,
-	LOG_INVALID_IDX,
-};
-
-int logger_write(const enum logidx index,
-		const unsigned char priority,
-		const char __kernel * const tag,
-		const char __kernel * const fmt,
-		...);
-
-#endif /* __KERNEL__ */
 
 #endif /* _LINUX_LOGGER_H */
